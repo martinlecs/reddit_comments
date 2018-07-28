fbe0ddeb2d434eae8dcaa58f032fa078848412c8
Martin Le <martinle.cse@gmail.com>
2018-07-28 23:43:04 +1000

    implemented basic RedditAPI consumer and unit tests

---
+++ tests/test_redditapi.py
@@ -0,0 +1,23 @@
+# Unit tests using Pytest testing framework
+import pytest
+from reddit_comments.redditapi import *
+
+
+@pytest.fixture
+def reddit():
+    return RedditAPI()
+
+
+def test_initialisation(reddit):
+    assert reddit.is_read_only
+
+
+@pytest.mark.parametrize("limit", [10,25,50,0])
+def test_get_hottest_posts(reddit, limit):
+    assert len([i for i in reddit.get_hottest_posts('anime', limit)]) == limit
+
+
+@pytest.mark.parametrize("limit, rounded", [(-1, 0), (1.5, 1), (.5, 1)])
+def test_get_hottest_posts_invalid_limits(reddit, limit, rounded):
+    with pytest.raises(InvalidLimitError):
+        assert [i for i in reddit.get_hottest_posts('anime', limit)] == rounded
